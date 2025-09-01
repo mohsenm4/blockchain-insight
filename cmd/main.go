39d8334c41e5c465c82ecac605c0321a867ca3fe
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2025-09-01 11:29:20 +0330

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/Mohsen20031203/blockchain-insight/internal/enthblock"
+import (
+	"log"
+
+	"github.com/Mohsen20031203/blockchain-insight/config"
+	"github.com/Mohsen20031203/blockchain-insight/internal/enthblock"
+)
 
 func main() {
-	enthblock.NewClient("https://your-rpc-url")
+
+	config, err := config.LoadConfig(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+	client, err := enthblock.NewClient(config.RPCURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = client
 }
