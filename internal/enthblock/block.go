add467387187967700c42890b0a6e0788d015e40
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2025-09-01 12:33:22 +0330

    Update block.go
    Add function to get latest block number #2

--- internal/enthblock/block.go
+++ internal/enthblock/block.go
@@ -1,1 +1,15 @@
 package enthblock
+
+import (
+	"context"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func GetLatestBlockNumber(client *ethclient.Client) (uint64, error) {
+	blockNumber, err := client.BlockNumber(context.Background())
+	if err != nil {
+		return 0, err
+	}
+	return blockNumber, nil
+}
