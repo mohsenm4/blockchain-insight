e5cbd48e4d7b542c28e6cdb87810511139ec105a
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2025-09-01 11:27:53 +0330

    add load config

--- internal/models/config.go
+++ internal/models/config.go
@@ -1,7 +1,1 @@
 package models
-
-type Config struct {
-	RPCURL      string `mapstructure:"rpc_url"`
-	NetworkName string `mapstructure:"network_name"`
-	TimeoutSec  int    `mapstructure:"timeout_sec"`
-}
