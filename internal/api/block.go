61ff5f86f88d61c4f57766be5815fe60e211cedf
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2025-09-03 11:48:43 +0330

    Update block.go

--- internal/api/block.go
+++ internal/api/block.go
@@ -7,6 +7,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// GetAddressBalance godoc
+// @Summary      Get balance of an address
+// @Description  Returns the balance of a given blockchain address
+// @Tags         address
+// @Accept       json
+// @Produce      json
+// @Param        address   path      string  true  "Blockchain address"
+// @Success      200  {object}  map[string]string
+// @Failure      500  {object}  map[string]string
+// @Router       /address/{address}/balance [get]
 func (s *Server) GetAddressBalance(c *gin.Context) {
 	address := c.Param("address")
 	balance, err := enthblock.GetBalance(s.client, address)
@@ -17,6 +27,17 @@ func (s *Server) GetAddressBalance(c *gin.Context) {
 	c.JSON(200, gin.H{"balance": balance.String()})
 }
 
+// GetBlockById godoc
+// @Summary      Get block by number
+// @Description  Returns the block information for the given block number
+// @Tags         block
+// @Accept       json
+// @Produce      json
+// @Param        id   path      int     true  "Block number"
+// @Success      200  {object}  models.Block
+// @Failure      400  {object}  map[string]string
+// @Failure      500  {object}  map[string]string
+// @Router       /block/{id} [get]
 func (s *Server) GetBlockById(c *gin.Context) {
 	id := c.Param("id")
 
@@ -33,6 +54,15 @@ func (s *Server) GetBlockById(c *gin.Context) {
 	c.JSON(200, block)
 }
 
+// GetLastBlock godoc
+// @Summary      Get the latest block
+// @Description  Returns the most recent block information from the blockchain
+// @Tags         block
+// @Accept       json
+// @Produce      json
+// @Success      200  {object}  models.Block
+// @Failure      500  {object}  map[string]string
+// @Router       /block/latest [get]
 func (s *Server) GetLastBlock(c *gin.Context) {
 
 	blockNumber, err := enthblock.GetLatestBlockNumber(s.client)
