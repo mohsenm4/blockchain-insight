c3489c15511deae21ce2e16fdd5d98c3c98e35c4
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2025-09-03 15:49:45 +0330

    add func initCache for cache
    #4

--- internal/api/cache.go
+++ internal/api/cache.go
@@ -1,1 +1,15 @@
 package api
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func (s *Server) initCache(c *gin.Context) {
+
+	block, ok := s.cach.Get("latest_block")
+	if ok {
+		c.JSON(200, block)
+		return
+	}
+
+}
