f0b5e1401520f9a9d7ed8f468f1ef8a5198833d8
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2025-09-04 10:03:21 +0330

    Update block.go

--- internal/api/block.go
+++ internal/api/block.go
@@ -7,26 +7,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// GetAddressBalance godoc
-// @Summary      Get balance of an address
-// @Description  Returns the balance of a given blockchain address
-// @Tags         block
-// @Accept       json
-// @Produce      json
-// @Param        address   path      string  true  "Blockchain address"
-// @Success      200  {object}  map[string]string
-// @Failure      500  {object}  map[string]string
-// @Router       /balace/{address} [get]
-func (s *Server) GetAddressBalance(c *gin.Context) {
-	address := c.Param("address")
-	balance, err := s.client.GetBalance(address)
-	if err != nil {
-		c.JSON(500, gin.H{"error": err.Error()})
-		return
-	}
-	c.JSON(200, gin.H{"balance": balance.String()})
-}
-
 // GetBlockById godoc
 // @Summary      Get block by number
 // @Description  Returns the block information for the given block number
