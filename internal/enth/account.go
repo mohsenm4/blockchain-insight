6d23915b06f39cd606f947a3cb30e47501891e1e
mohsen <mohsen.salehi2070@gmail.com>
2025-11-09 22:13:51 +0330

    Update account.go

--- internal/enth/account.go
+++ internal/enth/account.go
@@ -7,6 +7,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
+// GetBalance retrieves the balance of the given Ethereum address.
 func (c *Client) GetBalance(address string) (*big.Int, error) {
 	account := common.HexToAddress(address)
 
